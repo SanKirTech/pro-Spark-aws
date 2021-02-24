cb0e15b5f299fd965a3a8f8ccd4ac3fb609c47dd
Sanjay.Bheemasenarao <sanjay.bheemasenarao@gmail.com>
2021-02-24 13:30:58 +0530

    Modified copyright

--- code/spark/src/main/scala/com/sankir/smp/common/Converter.scala
+++ code/spark/src/main/scala/com/sankir/smp/common/Converter.scala
@@ -1,4 +1,19 @@
 /*
+ *
+ *  * SanKir Technologies
+ *  * (c) Copyright 2021.  All rights reserved.
+ *  * No part of pro-Spark course contents - code, video or documentation - may be reproduced, distributed or transmitted
+ *  *  in any form or by any means including photocopying, recording or other electronic or mechanical methods,
+ *  *  without the prior written permission from Sankir Technologies.
+ *  *
+ *  * The course contents can be accessed by subscribing to pro-Spark course.
+ *  *
+ *  * Please visit www.sankir.com for details.
+ *  *
+ *
+ */
+
+/*
  * Comment for pipeline module
  * Scala objects
  * Spark sql table
