3a491517e2ce9dae4165e6c4cd38d4f4afbb64c1
Sanjay.Bheemasenarao <sanjay.bheemasenarao@gmail.com>
2021-04-20 15:13:30 +0530

    fixed k5temp

--- infrastructure/load_summary/k5_summary.sql
+++ infrastructure/load_summary/k5_summary.sql
@@ -10,18 +10,18 @@ WHEN NOT MATCHED BY TARGET
     THEN INSERT (customerid, revenue, rank)
          VALUES(customerid, revenue, 0);
 
-drop table retail_kpi_summary.kpi5temp;
+drop table if exists retail_kpi_summary.k5temp;
 
-create table retail_kpi_summary.kpi5temp as SELECT customerid, revenue, rank() over (order by revenue desc) as rank
+create table retail_kpi_summary.k5temp as SELECT customerid, revenue, rank() over (order by revenue desc) as rank
 FROM `pro-spark.retail_kpi_summary.t_customer_rank_summary` group by customerid,revenue order by 3; 
 
 update pro-spark.retail_kpi_summary.t_customer_rank_summary a 
 set rank = b.rank
-    from `pro-spark.retail_kpi_summary.kpi5temp`b
+    from `pro-spark.retail_kpi_summary.k5temp` b
     where a.customerid = b.customerid;
 
 select * from pro-spark.retail_kpi_summary.t_customer_rank_summary order by rank;
 
-drop table retail_kpi_summary.kpi5temp;
+drop table if exists retail_kpi_summary.k5temp;
 
 end
