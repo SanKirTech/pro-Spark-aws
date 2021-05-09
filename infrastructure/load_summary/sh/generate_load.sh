9b4203c117008f0720514c86a8f9596074def276
Sanjay.Bheemasenarao <sanjay.bheemasenarao@gmail.com>
2021-05-09 23:11:40 +0530

    deleted load script as its generated one and also staged latest generate script

--- infrastructure/load_summary/sh/generate_load.sh
+++ infrastructure/load_summary/sh/generate_load.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 
+if [ -z "$1" ]; then
+        echo "gs URI is not provided as argument. exiting"
+        echo "example run is:"
+        echo "./generate_load.sh gs://sankir-storage-prospark/kpi/load_summary/sql/*.sql"
+        exit -1
+fi
+
 echo "#!/bin/bash" > $HOME/load_summary.sh
 echo "# Summary Tables load scripts" >> $HOME/load_summary.sh
 
 #for i in `gsutil ls gs://sankir-storage-prospark/kpi/load_summary/*.sql`
 for i in `gsutil ls $1`
-do 
+do
 
  echo "gsutil cat $i  | bq query --use_legacy_sql=false" >> $HOME/load_summary.sh
 
