e38f798912aade06ea9a39c647bb6706b7747cc4
Sanjay.Bheemasenarao <sanjay.bheemasenarao@gmail.com>
2021-05-10 20:19:19 +0530

    deleted sqls which are not in use

--- infrastructure/kpi/check_results.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM `pro-spark.retail_kpi.t_customer_rank_dly` LIMIT 1000;
-SELECT * FROM `pro-spark.retail_kpi.t_revenue_country_dly` LIMIT 1000;
-SELECT * FROM `pro-spark.retail_kpi.t_revenue_country_qtr_dly` LIMIT 1000;
-SELECT * FROM `pro-spark.retail_kpi.t_revenue_qtr_dly` LIMIT 1000;
-SELECT * FROM `pro-spark.retail_kpi.t_sales_anomoly_dly` LIMIT 1000;
-SELECT * FROM `pro-spark.retail_kpi.t_sku_dow_daily` LIMIT 1000;
-SELECT * FROM `pro-spark.retail_kpi.t_sku_revenue_c_dly` LIMIT 1000;
-SELECT * FROM `pro-spark.retail_kpi.t_sku_revenue_w_dly` LIMIT 1000;
